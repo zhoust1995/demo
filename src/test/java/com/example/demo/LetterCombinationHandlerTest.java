095073487601fbfa7d03d56cb3a51f7345ebc05f
zhoust <1010469455@qq.com>
2020-10-09 23:32:47 +0800

    fix

--- src/test/java/com/example/demo/LetterCombinationHandlerTest.java
+++ src/test/java/com/example/demo/LetterCombinationHandlerTest.java
@@ -10,11 +10,6 @@ import java.util.List;
  * @author: zhoust
  * @Description:
  * @Date: 2020/10/9
- * ******************************************************************************
- * Copyright (C)  CSG Information Technology Co.,Ltd
- * All Rights Reserved.
- * 本软件为鼎信信息科技有限责任公司开发研制。未经本公司正式书面同意，其他任何个人、团体不得使用、复制、修改或发布本软件.
- * *****************************************************************************
  */
 @SpringBootTest
 public class LetterCombinationHandlerTest {
